ba0652fbb3da9cf0fc57309a0357193891778db6
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2013-12-10 11:47:21 +0530

    Cleaned

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+
+//------------------------------
+//
+// 2013-12-10, Prabhat Khera
+//
+// Copyright Prabhat Khera
+//
+// Install with dependencies: npm install 
+//
+// Documentation is 'docco style' - http://jashkenas.github.com/docco/
+//
+// Using Google JavaScript Style Guide - http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml
+//
+//------------------------------
+
+
+"use strict";
+
+
+// Includes
+// =========
+
 var express = require('express');
 var http = require('http');
 var path = require('path');
--- models/user.js
+++ models/user.js
@@ -1,3 +1,18 @@
+
+//------------------------------
+//
+// 2013-12-10, Prabhat Khera
+//
+// Copyright Prabhat Khera
+//
+// Install with dependencies: npm install 
+//
+// Documentation is 'docco style' - http://jashkenas.github.com/docco/
+//
+// Using Google JavaScript Style Guide - http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml
+//
+//------------------------------
+
 var mongoose = require('mongoose'), Schema = mongoose.Schema;
 
 var userSchema = new Schema({
